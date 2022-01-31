d62202d1a96561fb43877f4e6ed854ffa589ab21
Brenon <brenonparry@gmail.com>
2022-01-31 21:06:21 +0000

    add a gets.chomp.to_i to capture integer value of user answer. Plus a condistional to puts an appropriate answer message.

--- question.rb
+++ question.rb
@@ -7,9 +7,20 @@ class Question
    
 
     puts "------ NEW TURN ------"
-    puts "Player1: What is #{rand(1..10)} + #{rand(1..10)}?"
+    random_number1 = rand(1..10)
+    random_number2 = rand(1..10)
+    correct_answer = random_number1 + random_number1
+    puts "Player1: What is #{random_number1} + #{random_number2}?"
     print "> "
-    
+    gets.chomp.to_i
 
+    
+    
+    unless gets.chomp.to_i == correct_answer
+      puts "Wrong answer ya bish"
+    end
+    if gets.chomp.to_i == correct_answer
+      puts "Ayyyyy dats right"
+    end
   end
 end

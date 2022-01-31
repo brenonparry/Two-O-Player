cec851e365a1a513dbcef7d33ca63c35f44c882b
Brenon <brenonparry@gmail.com>
2022-01-31 23:22:35 +0000

    move ---- NEW TURN ---- logic to game class

--- question.rb
+++ question.rb
@@ -1,26 +1,30 @@
+require './game'
+
 class Question
+  attr_reader :random_number1, :random_number2, :correct_answer
   def initialize
-
+    @random_number1 = rand(1..10)
+    @random_number2 = rand(1..10)
+    @correct_answer = @random_number1 + @random_number2
+    # puts "Player1: What is #{@random_number1} + #{@random_number2}?"
+    # puts "+++++++++++++++++"
+    # puts @correct_answer
   end
 
-  def new_turn
+
+  # def new_turn
    
+  #   puts "------ NEW TURN ------"
+  #   puts "Player1: What is #{random_number1} + #{random_number2}?"
+  #   print "> " 
+  #   player_answer = gets.chomp.to_i
+  
 
-    puts "------ NEW TURN ------"
-    random_number1 = rand(1..10)
-    random_number2 = rand(1..10)
-    correct_answer = random_number1 + random_number1
-    puts "Player1: What is #{random_number1} + #{random_number2}?"
-    print "> "
-    gets.chomp.to_i
-
-    
-    
-    unless gets.chomp.to_i == correct_answer
-      puts "Wrong answer ya bish"
-    end
-    if gets.chomp.to_i == correct_answer
-      puts "Ayyyyy dats right"
-    end
-  end
+  #   unless player_answer == correct_answer
+  #     puts "Wrong answer ya bish"
+  #   end
+  #   if player_answer == correct_answer
+  #     puts "Ayyyyy dats right"
+  #   end
+  # end
 end

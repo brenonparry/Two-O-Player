3860040329be5fa221264b5134bdec88988a0cba
Brenon <brenonparry@gmail.com>
2022-01-31 20:51:51 +0000

    create Question class with a def new_turn which puts the begining of a new turn

---
+++ question.rb
@@ -0,0 +1,15 @@
+class Question
+  def initialize
+
+  end
+
+  def new_turn
+   
+
+    puts "------ NEW TURN ------"
+    puts "Player1: What is #{rand(1..10)} + #{rand(1..10)}?"
+    print "> "
+    
+
+  end
+end
